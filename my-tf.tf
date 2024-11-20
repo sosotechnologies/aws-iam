a9eded313aec5f234b0ae1504664ebd54153772b
Collins Afanwi <117014550+sosotechnologies@users.noreply.github.com>
2024-11-20 15:09:20 -0500

    Update my-tf.tf

--- my-tf.tf
+++ my-tf.tf
@@ -1,118 +1,279 @@
+## THIS TO AUTHENTICATE TO ECR, DON'T CHANGE IT
 provider "aws" {
   region = "us-east-1"
+  alias  = "virginia"
 }
 
-# Create IAM Policy to allow assuming the admin-weekend role
-module "iam_policy_assume_admin" {
-  source      = "../../modules/iam-policy"
-  
-  create_policy = true
-  name          = "AllowAssumeAdminWeekend"
-  description   = "Policy to allow assuming the admin-weekend role"
-  policy        = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = "sts:AssumeRole",
-        Resource = "arn:aws:iam::368085106192:role/admin-weekend"  
-      }
-    ]
-  })
-  tags = {
-    Environment = "dev"
-    Project     = "my-project"
+provider "aws" {
+  region = local.region
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
   }
 }
 
-# Create the admin-weekend role with a trust policy
-module "iam_assumable_role_admin_weekend" {
-  source                     = "../../modules/iam-assumable-role"
+provider "kubectl" {
+  apply_retry_count      = 10
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+  token                  = data.aws_eks_cluster_auth.this.token
+}
 
-  create_role               = true
-  role_name                 = "admin-weekend"
-  role_requires_mfa         = false 
-  trusted_role_arns         = [
-    "arn:aws:iam::307990089504:root",
-    "arn:aws:iam::368085106192:user/terraform-admin"
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_ecrpublic_authorization_token" "token" {
+  provider = aws.virginia
+}
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+locals {
+  name            = "mimeo-production-cluster"
+  cluster_version = "1.31"
+  region          = var.region
+  node_group_name = "managed-ondemand"
+  node_iam_role_name = module.eks_blueprints_addons.karpenter.node_iam_role_name
+
+  vpc_id              = module.vpc.vpc_id
+  vpc_cidr            = module.vpc.vpc_cidr_block
+  public_subnets_ids  = module.vpc.public_subnets
+  private_subnets_ids = module.vpc.private_subnets
+  database_subnets_id = module.vpc.database_subnets
+  subnets_ids         = concat(local.public_subnets_ids, local.private_subnets_ids, local.database_subnets_id)
+
+  # vpc_cidr = "10.0.0.0/16"
+  ## NOTE: You might need to change this less number of AZs depending on the region you're deploying to
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+  secondary_cidr = "10.1.0.0/16"  # secondary
+  tags = {
+    blueprint = local.name
+  }
+}
+
+################################################################################
+# Cluster
+################################################################################
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.23.0"
+
+  cluster_name                   = local.name
+  cluster_version                = local.cluster_version
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    kube-proxy = { most_recent = true }
+    coredns    = { most_recent = true }
+
+    vpc-cni = {
+      most_recent    = true
+      before_compute = true
+      configuration_values = jsonencode({
+        env = {
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = local.private_subnets_ids
+
+  create_cloudwatch_log_group              = false
+  create_cluster_security_group            = false
+  create_node_security_group               = false
+  authentication_mode                      = "API_AND_CONFIG_MAP"
+  enable_cluster_creator_admin_permissions = true
+  
+  
+  eks_managed_node_groups = {
+    mg_5 = {
+      node_group_name = "managed-ondemand"
+      instance_types  = ["t2.medium", "t3.medium", "m5.large"]
+ 
+      create_security_group = false
+      subnet_ids   = module.vpc.private_subnets
+      max_size     = 2
+      desired_size = 2
+      min_size     = 2
+
+      # Launch template configuration
+      create_launch_template = true              # false will use the default launch template
+      launch_template_os     = "amazonlinux2eks" # amazonlinux2eks or bottlerocket
+      
+      key_name = "kapistio-key-2025"
+
+      labels = {
+        intent = "control-apps"
+      }
+    }
+  }
+
+  tags = merge(local.tags, {
+    "karpenter.sh/discovery" = local.name
+  })
+
+  ####### collins add rules for istio  ##################################
+  node_security_group_additional_rules = {
+    ingress_15017 = {
+      description                   = "Cluster API - Istio Webhook namespace.sidecar-injector.istio.io"
+      protocol                      = "TCP"
+      from_port                     = 15017
+      to_port                       = 15017
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+    ingress_15012 = {
+      description                   = "Cluster API to nodes ports/protocols"
+      protocol                      = "TCP"
+      from_port                     = 15012
+      to_port                       = 15012
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+    ssh_22 = { # Allow SSH access on port 22
+      description                   = "SSH access to worker nodes"
+      protocol                      = "TCP"
+      from_port                     = 22
+      to_port                       = 22
+      type                          = "ingress"
+      cidr_blocks                   = ["0.0.0.0/0"] # Adjust to restrict access as needed
+    }
+  }
+  ####### added  rules for istio above  #########################################
+}
+
+# resource "aws_key_pair" "mimeo_ssh_key" {
+#   key_name   = "kapistio-key-2025"
+#   public_key = file("../keys/kapistio-key-2025.pem")
+# }
+
+module "eks_blueprints_addons" {
+  # source  = "aws-ia/eks-blueprints-addons/aws"
+  source  = "../modules/eks-addons"
+  # version = "1.16.3"
+
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  create_delay_dependencies = [for prof in module.eks.eks_managed_node_groups : prof.node_group_arn]
+  
+  enable_aws_load_balancer_controller = true
+  enable_metrics_server               = true
+  
+  enable_argocd                                = true
+  enable_argo_rollouts                         = true
+  enable_argo_workflows                        = true
+
+  eks_addons = {
+  aws-ebs-csi-driver = {
+    service_account_role_arn = module.mimeo-ebs_csi_driver.iam_role_arn
+  }
+}
+
+  enable_karpenter = true
+
+  karpenter = {
+    chart_version       = "1.0.1"
+    repository_username = data.aws_ecrpublic_authorization_token.token.user_name
+    repository_password = data.aws_ecrpublic_authorization_token.token.password
+  }
+  karpenter_enable_spot_termination          = true
+  karpenter_enable_instance_profile_creation = true
+  karpenter_node = {
+    iam_role_use_name_prefix = false
+  }
+
+  tags = local.tags
+}
+
+module "mimeo-ebs_csi_driver" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.44.0"
+
+  role_name_prefix = "${module.eks.cluster_name}-ebs-driver-"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+
+  tags = local.tags
+}
+
+module "aws-auth" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "~> 20.0"
+
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = module.eks_blueprints_addons.karpenter.node_iam_role_arn
+      username = "system:node:{{EC2PrivateDNSName}}"
+      groups   = ["system:bootstrappers", "system:nodes"]
+    },
   ]
-  admin_role_policy_arn     = "arn:aws:iam::aws:policy/AdministratorAccess"
-  tags                      = {
-    Environment = "dev"
-    Project     = "my-project"
+}
+
+#---------------------------------------------------------------
+# Supporting Resources
+#---------------------------------------------------------------
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.0"
+
+  name = "mimeo-prod-vpc"
+  cidr = "10.0.0.0/16"
+  secondary_cidr_blocks = [local.secondary_cidr]
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.16.0/22", "10.0.20.0/22", "10.0.24.0/22"]
+  public_subnets  = ["10.0.200.0/22", "10.0.204.0/22", "10.0.208.0/22"]
+  database_subnets     = ["10.0.80.0/24", "10.0.81.0/24", "10.0.82.0/24", "10.1.16.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_vpn_gateway = false
+
+
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/elb"              = 1
   }
-}
-##################
 
-# Policy for S3 access (attach to the existing role)
-resource "aws_iam_policy" "s3_access_policy" {
-  name        = "S3AccessPolicy"
-  description = "Policy to allow listing and reading from S3 buckets"
-  
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:ListAllMyBuckets",
-          "s3:ListBucket",
-          "s3:GetObject"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Attach the S3 access policy to the admin-weekend role
-resource "aws_iam_role_policy_attachment" "attach_s3_access_policy" {
-  role       = "admin-weekend"  # Attach to the admin-weekend role
-  policy_arn = aws_iam_policy.s3_access_policy.arn
-}
-
-# Additional policy for frontenders
-module "iam_policy_frontenders" {
-  source      = "../../modules/iam-policy"
-  
-  create_policy = true
-  name          = "AllowFrontendersToAssumeRole"
-  description   = "Policy for Frontenders"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:ListAllMyBuckets",
-          "s3:ListBucket",
-          "s3:GetObject"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-  tags = {
-    Environment = "dev"
-    Project     = "my-project"
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/internal-elb"     = 1
+    "karpenter.sh/discovery"              = local.name
   }
-}
 
-# Attach the frontenders S3 access policy to the admin-weekend role
-resource "aws_iam_role_policy_attachment" "attach_frontenders_policy" {
-  role       = "admin-weekend"
-  policy_arn = module.iam_policy_frontenders.arn
+  tags = local.tags
 }
-################
-# outputs.tf
-output "terraform_user_role_arn" {
-  description = "The ARN of the Terraform user role"
-  value       = module.iam_assumable_role_admin_weekend.iam_role_arn  # Update to match the correct module name
-}
-
-output "terraform_policy_arn" {
-  description = "The ARN of the policy for assuming the admin-weekend role"
-  value       = module.iam_policy_assume_admin.arn  # Update to match the correct module name
-}
-
 
